b1357061ae42843e49bf83861a8cb356e76ef1a9
Torbjørn Jørgensen <torbjorn@TORBJORGENSEN.NO>
2018-11-12 11:18:48 +0100

    new file:   Mail-VMsWithSnapshots.ps1 	new file:   Torbjorn.VMWare.psd1 	new file:   Torbjorn.VMWare.psm1 	new file:   headerTemplate.html

---
+++ Mail-VMsWithSnapshots.ps1
@@ -0,0 +1,61 @@
+<#
+    
+    .SYNOPSIS
+        Sends mail to someone when there are VMs with snapshots on a vCenter Server
+
+    .DESCRIPTION
+        Get list of Virtual Machines on a vCenter Server that have snapshots. If there are VMs with snapshots, send warning via e-mail.
+    
+    .NOTES
+        Remember to configure the script by editing the $Config hashtable in the beginning of the script
+    
+#>
+
+BEGIN {
+
+    $Config = @{
+        
+        vcsaServer="vcsa.torbjorgensen.no"
+        emailSender="powershell@torbjorgensen.no"
+        emailRepicent="torb.jorgensen@gmail.com"    
+        smtpServer="smtp.ntebb.no"
+        headerFile="headerTemplate.html"
+
+    }
+
+}
+
+
+PROCESS {
+
+    try {
+        Import-Module Torbjorn.VMWare
+
+        $vms = Get-VMsWithSnapshots -Server $Config.vcsaServer | Where-Object Count -gt 0
+        $vmCount = $vms.Count
+
+        if ($vmCount)
+        {
+            $vcsaServer = $Config.vcsaServer
+            $emailRepicent = $Config.emailRepicent
+            $time = Get-Date
+
+            $Message = "There are $vmCount virtual machines with snapshots taking up valuable disk space!<p>"
+            $Footer = "Collected from $vcsaServer at $time"
+            $Subject = "VM Snapshot information"
+            $Head = [string](Get-Content $Config.headerFile)
+            $Body = [string]($vms | ConvertTo-Html -PreContent $Message -PostContent $Footer -Title $Subject -Head $Head)
+
+            Send-MailMessage -To $Config.emailRepicent -From $Config.emailSender -Subject $Subject -BodyAsHtml -Body $Body -SmtpServer $Config.smtpServer
+            Write-Host "Successfully sent mail to $emailRepicent with information which $vmCount VMs has snapshots."
+        } else {
+            Write-Host "No VMs have snapshots."  
+        }
+    } catch {
+        Write-Error "There were errors occuring while running script..."
+    }
+}
+
+END {
+
+}
---
+++ Torbjorn.VMWare.psm1
@@ -0,0 +1,99 @@
+<#
+    
+    .SYNOPSIS
+        This function retrieves the amount of snapshots virtual machines on a vCenter Server system have.
+
+    .DESCRIPTION
+        This function retrieves the amount of snapshots for all virtual machines on a vCenter Server system. Returns VM Name, Host, 
+        VM Folder, Power state and Snapshot count.
+
+    .EXAMPLE
+        Get-VMsWithSnapshots vcsa.example.com
+        Get a list of all VMs on the vCenter Server vcsa.example.com
+
+    .EXAMPLE
+        Get-VMsWithSnapshots -Server vcsa.example.com | Where-Object Count -gt 0
+        Get a list of all VMs on the vCenter Server vcsa.example.com that have snapshots
+    
+#>
+
+function Get-VMsWithSnapshots {
+    [CmdletBinding()]
+    param (
+        [parameter( Mandatory=$True,
+                    ValueFromPipeline=$True,
+                    ValueFromPipelineByPropertyName=$True,
+                    HelpMessage="IP or Hostname to vCenter server" )]
+        [string]$Server
+    )
+
+    BEGIN {
+        # open connection to vcenter server
+        try {
+            Write-Verbose "Opening connection to vCenter Server at $Server"
+            $vcsaConnection = Connect-VIServer -Server $Server -ErrorAction stop
+        } catch {
+            Write-Error "Could not connect to vCenter server at $Server"
+        }
+    }
+
+    PROCESS {
+        if ($Null -ne $vcsaConnection)
+        {
+            Write-Verbose "Getting list of VMs..."
+            $vms = Get-VM
+
+            foreach ($vm in $vms) {
+                try {
+                    Write-Verbose "Checking $vm for snapshots..."
+                    $result = Get-Snapshot $vm.Name
+                    $count  = Measure-Object -InputObject $result | Select-Object -expand Count
+
+                    if ($count -is [int32] -and $count -gt 0)
+                    {
+                        # VM have snapshots
+                        Write-Verbose " * $vm has $count snapshots"
+                        $result = @{Name = $vm
+                                    VMHost = $vm.VMHost
+                                    Folder = $vm.Folder
+                                    PowerState = $vm.PowerState
+                                    Count = $count}
+                    } else {
+                        # VM have no snapshots
+                        $result = @{Name = $vm
+                                    VMHost = $vm.VMHost
+                                    Folder = $vm.Folder
+                                    PowerState = $vm.PowerState
+                                    Count = 0}
+                    }
+                } catch {
+                    # unable to get snapshot information
+                    $result = @{Name = $vm
+                                VMHost = $null
+                                Folder = $null
+                                PowerState = $null
+                                Count = 0}
+                } finally {
+                    $obj = New-Object -TypeName PSObject 
+                    $obj | Add-Member -MemberType NoteProperty -Name Name -value $result.Name
+                    $obj | Add-Member -MemberType NoteProperty -Name Count -value $result.Count
+                    $obj | Add-Member -MemberType NoteProperty -Name VMHost -value $result.VMHost
+                    $obj | Add-Member -MemberType NoteProperty -Name Folder -value $result.Folder
+                    $obj | Add-Member -MemberType NoteProperty -Name PowerState -value $result.PowerState
+
+                    Write-Output $obj
+                }
+            }
+        }
+    }
+
+    END {
+        try {
+            Write-Verbose "Diconnecting from vcenter server"
+            Disconnect-VIServer -Server $vcsaConnection -Confirm:$False
+        } catch {
+            Write-Error "Unable to disconnect from vCenter server"
+        }
+    }
+}
+
